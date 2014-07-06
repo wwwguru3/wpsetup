e58a978d72ad27b924f20d0c24593b13171204b8
wwwguru3 <wwwguru3@gmail.com>
2014-07-05 18:19:56 -0700

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'phpwork');
 
-/** MySQL database username */
-define('DB_USER', 'root');
-
-/** MySQL database password */
-define('DB_PASSWORD', 'hambo2zk');
-
-/** MySQL hostname */
-define('DB_HOST', '192.168.0.10');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
